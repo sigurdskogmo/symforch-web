e94acaf463d79537e9d6b93d0abd0caad4e5de36
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-09-25 12:00:21 +0200

    Add address

--- src/views/AboutView/index.js
+++ src/views/AboutView/index.js
@@ -95,7 +95,12 @@ const AboutView = () => {
                                     <b>E-post:</b> <a href='mailto:symforch@samfundet.no'>symforch@samfundet.no</a><br />
                                     <b>Telefon:</b> 941 95 440 (Andreas Klavenes Berg)<br />
                                     <b>Facebook:</b> <a href='https://www.facebook.com/symforch'>www.facebook.com/symforch</a><br />
-                                    <b>Instagram:</b> <a href='https://www.instagram.com/symforch/'>www.instagram.com/symforch/</a>
+                                    <b>Instagram:</b> <a href='https://www.instagram.com/symforch/'>www.instagram.com/symforch/</a><br /><br />
+                                    <b>Adresse:</b><br />
+                                    Studentersamfundets Symfoniorkester<br />
+                                    Studentersamfundet i Trondhjem<br />
+                                    Elgesetergate 1<br />
+                                    7030 TRONDHEIM<br />
                                 </Typography>
                                 <br />
                                 <Logo className={classes.logo}/>               

79fdf0ed462dc4f95362be41178f33aee97184b9
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-11-03 21:20:58 +0100

    Add tsjaikovskij concert

--- src/views/ConcertView/index.js
+++ src/views/ConcertView/index.js
@@ -10,6 +10,7 @@ import Container from '@material-ui/core/Container';
 // Components
 import Img from '../../components/Img';
 import festkonsert_banner from '../../assets/festkonsert_banner.jpg';
+import tsjaikovskij_banner from '../../assets/tsjaikovskij_banner.png';
 import Video from '../../components/Video';
 
 //import Copyright from '../../components/Copyright';
@@ -39,7 +40,8 @@ const useStyles = makeStyles((theme) => ({
       height: 240,
     },
     img: {
-        maxWidth: '100%'
+        width: '100%',
+        maxWidth: '100%',
     },
     canceled: {
         textDecoration: 'line-through'
@@ -61,15 +63,24 @@ const ConcertView = () => {
                                     Førstkommende konsert
                                 </Typography>
                                 <Typography component="p" variant="h5">
-                                    Konsert på Studentersamfundet
+                                    Tsjaikovskijs symfoni nr. 5
                                 </Typography>
-                                {/*<Img source={festkonsert_banner} alt={'Bannerplakat for festkonsert'} className={classes.img}/>*/}
+                                <Img source={tsjaikovskij_banner} alt={'Bannerplakat for Tsjaikovski konsert'} className={classes.img}/>
                                 <Typography component="p" variant="body1">
-                                    <b>Når: </b>24. november<br />
+                                    <b>Når: </b>24. november kl. 19:00<br />
                                     <b>Hvor: </b>Storsalen, Studentersamfundet<br />
                                     <b>Hva: </b>
-                                    Tchaikovskys 5. symfoni presenteres av Studentersamfundets Symfoniorkester tirsdag 24. november!<br/>
-                                    Mer info kommer snart.
+                                    Velkommen til konsert med Studentersamfundets Symfoniorkester 24. november kl 19:00!<br/><br/>
+                                    
+                                    Denne gangen skal vi spille Tsjaikovskijs 5. symfoni. Symfonien er konstruert etter samme 
+                                    modell som Beethovens femte, der temaet går fra kamp til seier, fra mørke til lys.<br/><br/>
+                                    
+                                    Dette eventyret vil du ikke gå glipp av. Bli med oss da vi fyller storsalen med en av de 
+                                    mer berømte symfoniene fra 1800-tallet.<br/><br/>
+
+                                    <strong>Billetter slippes snarest!</strong><br/>
+                                    Student / barn: 80kr<br/>
+                                    Ordinær: 150kr
                                 </Typography>             
                             </Paper>
                         </Grid>
@@ -86,7 +97,7 @@ const ConcertView = () => {
                                     canceled
                                 />
                                 <UpcomingItem 
-                                    title={"Konsert i Storsalen"}
+                                    title={"Tsjaikovskijs symfoni nr. 5"}
                                     date={"24. november"}
                                     location={"Storsalen, Studentersamfundet"}
                                 />

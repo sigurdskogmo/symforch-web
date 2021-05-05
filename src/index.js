a715f3813f1970ce86beb5735b02c72937a35315
sigurdskogmo <sigurd.skogmo@gmail.com>
2021-05-05 21:57:40 +0200

    Experiment with dark-mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import './index.css';
 import App from './App';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
+import { darkTheme } from './theme';
+
+let dark = false;
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme = {dark ? darkTheme : theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
--- src/theme.js
+++ src/theme.js
@@ -4,15 +4,43 @@ const theme = createMuiTheme({
     palette: {
         primary: {
             main: '#ee1d23', // The same color that the logo has
+            contrastText: '#000000'
+
         },
         secondary: {
             main: '#ffffff',
-            contrastText: 'red'
+            contrastText: '#000000'
         },
+        background: {
+            default: '#f6f7f9',
+            paper: '#ffffff'
+        },
+        action: {
+            hover: 'rgba(255, 255, 255, 0.08)',
+            selcected: 'rgba(255, 255, 255, 0.16)'
+        }
     },
     status: {
         danger: 'red',
     }
 });
+
+export const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+      primary: {
+          main: '#ee1d23',
+          contrastText: '#ffffff'
+      },
+      secondary: {
+          main : '#444444',
+          contrastText: '"ffffff"'
+      },
+      action: {
+          hover: 'rgba(255, 255, 255, 0.08)',
+          selcected: 'rgba(255, 255, 255, 0.16)'
+      }
+    },
+  });
 
 export default theme;
